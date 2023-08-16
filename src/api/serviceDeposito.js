ecabec8a2d8b73306cb2096ad7ae707d648620cb
Wesley Melo mota <wesleymota382@gmail.com>
2023-08-16 20:35:56 -0300

    add serviceDeposito e alterando formulario

--- src/api/serviceDeposito.js
+++ src/api/serviceDeposito.js
@@ -1,1 +1,8 @@
-/*implementar */
+import axios from "axios";
+
+function serviceDeposito(nomeResponsavelConta, numeroConta, valor, token){
+    return axios.post('http://localhost:8080/banco/transaction/deposito', {nomeResponsavelConta, numeroConta, valor}, {headers: {Authorization: token}})
+    
+}
+
+export default serviceDeposito;
